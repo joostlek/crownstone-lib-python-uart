809b44b00eefe549f087c90ef5859467246de857
alexdm0 <alexdemulder@gmail.com>
2020-05-29 15:08:33 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crownstone-uart',
-    version='0.5.3',
+    version='0.6.0',
     packages=find_packages(exclude=["examples","testing"]),
     author="Crownstone B.V.",
     long_description=long_description,
