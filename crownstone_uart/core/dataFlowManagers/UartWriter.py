c32fd38a13f9671ca22ac73c3ab59bee4ff97c85
alexdm0 <alexdemulder@gmail.com>
2020-09-10 13:07:17 +0200

    refactored uart write

--- crownstone_uart/core/dataFlowManagers/UartWriter.py
+++ crownstone_uart/core/dataFlowManagers/UartWriter.py
@@ -13,13 +13,9 @@ _LOGGER = logging.getLogger(__name__)
 
 class UartWriter:
 
-    def __init__(self, dataToSend: List[int], wait_until_result = 1, success_codes=None, interval = 0.001):
-        if success_codes is None:
-            success_codes = [ResultValue.SUCCESS]
-        self.success_codes = success_codes
+    def __init__(self, dataToSend: List[int], interval = 0.001):
         self.dataToSend : List[int] = dataToSend
         self.interval = interval
-        self.timeout = wait_until_result
 
         self.error   = None
         self.success = False
@@ -48,40 +44,49 @@ class UartWriter:
         if data == self.dataToSend:
             self.success = True
 
-    async def send_with_result(self) -> ResultPacket:
+    async def send_with_result(self, success_codes=None, wait_until_result=1) -> ResultPacket:
         UartEventBus.emit(SystemTopics.uartWriteData, self.dataToSend)
         counter = 0
-        while counter < self.timeout:
+        while counter < wait_until_result:
             if self.result:
-                if self.result.resultCode in self.success_codes or self.success_codes == []:
+                if success_codes is None or success_codes == [] or self.result.resultCode in success_codes:
                     self.__del__()
                     return self.result
                 else:
-                    raise CrownstoneException("WRITE_EXCEPTION", "Incorrect result type. Got " + str(
-                        self.result.resultCode) + ", expected one of " + str(self.success_codes), 400)
+                    raise CrownstoneException(
+                        "WRITE_EXCEPTION",
+                        "Incorrect result type. Got " + str(self.result.resultCode) + ", expected one of " + str(
+                            success_codes),
+                        400
+                    )
 
             await asyncio.sleep(self.interval)
             counter += self.interval
 
         self.__del__()
-        raise CrownstoneException("WRITE_EXCEPTION", "No result received after writing to UART. Waited for " + str(self.timeout) + " seconds", 404)
+        raise CrownstoneException("WRITE_EXCEPTION", "No result received after writing to UART. Waited for " + str(wait_until_result) + " seconds", 404)
 
-    def send_with_result_sync(self) -> ResultPacket:
+    def send_with_result_sync(self, success_codes=None, wait_until_result=1) -> ResultPacket:
         UartEventBus.emit(SystemTopics.uartWriteData, self.dataToSend)
         counter = 0
-        while counter < self.timeout:
+        while counter < wait_until_result:
             if self.result:
-                if self.result.resultCode in self.success_codes or self.success_codes == []:
+                if success_codes is None or success_codes == [] or self.result.resultCode in success_codes:
                     self.__del__()
                     return self.result
                 else:
-                    raise CrownstoneException("WRITE_EXCEPTION", "Incorrect result type. Got " + str(self.result.resultCode) + ", expected one of " + str(self.success_codes), 400)
+                    raise CrownstoneException(
+                        "WRITE_EXCEPTION",
+                        "Incorrect result type. Got " + str(self.result.resultCode) + ", expected one of " + str(success_codes),
+                        400
+                    )
 
             time.sleep(self.interval)
             counter += self.interval
 
         self.__del__()
-        raise CrownstoneException("WRITE_EXCEPTION", "No result received after writing to UART. Waited for " + str(self.timeout) + " seconds", 404)
+        raise CrownstoneException("WRITE_EXCEPTION", "No result received after writing to UART. Waited for " + str(wait_until_result) + " seconds", 404)
+
 
     async def send(self) -> None or ResultPacket:
         UartEventBus.emit(SystemTopics.uartWriteData, self.dataToSend)
