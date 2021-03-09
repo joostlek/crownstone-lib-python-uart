ba2ed50b77d0f24684bd813e72eb0aeab4629f32
alexdm0 <alexdemulder@gmail.com>
2021-03-09 15:29:52 +0100

    version bump for release 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crownstone-uart',
-    version='1.0.0',
+    version="1.0.1",
     packages=find_packages(exclude=["examples","testing"]),
     author="Crownstone B.V.",
     long_description=long_description,
