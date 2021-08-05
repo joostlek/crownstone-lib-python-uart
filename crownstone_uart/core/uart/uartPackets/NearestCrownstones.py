c5cf9a60e0f0fd11ec17f1056cdc1dfe6d8b40be
vliedel <bart@crownstone.rocks>
2021-08-05 09:25:03 +0200

    Updated nearest crownstone packet, as PacketBase was removed.

--- crownstone_uart/core/uart/uartPackets/NearestCrownstones.py
+++ crownstone_uart/core/uart/uartPackets/NearestCrownstones.py
@@ -1,15 +1,41 @@
-from crownstone_core.util.BasePackets import *
-import sys
+from crownstone_core.packets.BasePacket import BasePacket
+from crownstone_core.util.BufferReader import BufferReader
 
-class NearestCrownstoneTrackingUpdate(PacketBase):
 
-    def __init__(self):
-        self.assetId      = PacketBaseList(cls=Uint8, len=3)
-        self.crownstoneId = Uint8()
-        self.rssi         = Int8()
-        self.channel      = Uint8()
+class NearestCrownstoneTrackingUpdate(BasePacket):
+    def __init__(self, data=None):
+        self.assetId      = 0
+        self.crownstoneId = 0
+        self.rssi         = 0
+        self.channel      = 0
 
-class NearestCrownstoneTrackingTimeout(PacketBase):
+        if data is not None:
+            self.parse(data)
 
-    def __init__(self):
-        self.assetId      = PacketBaseList(cls=Uint8, len=3)
+    def _parse(self, reader: BufferReader):
+        self.assetId      = reader.getUInt8() + (reader.getUInt8() << 8) + (reader.getUInt8() << 16)
+        self.crownstoneId = reader.getUInt8()
+        self.rssi         = reader.getInt8()
+        self.channel      = reader.getUInt8()
+
+    def __str__(self):
+        return f"NearestCrownstoneTrackingUpdate(" \
+               f"assetId={self.assetId} " \
+               f"crownstoneId={self.crownstoneId} " \
+               f"rssi={self.rssi} " \
+               f"channel={self.channel})"
+
+
+class NearestCrownstoneTrackingTimeout(BasePacket):
+    def __init__(self, data=None):
+        self.assetId      = 0
+
+        if data is not None:
+            self.parse(data)
+
+    def _parse(self, reader: BufferReader):
+        self.assetId      = reader.getUInt8() + (reader.getUInt8() << 8) + (reader.getUInt8() << 16)
+
+    def __str__(self):
+        return f"NearestCrownstoneTrackingTimeout(" \
+               f"assetId={self.assetId})"
