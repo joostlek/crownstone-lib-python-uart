14211a10be1c6dff16563fb2fc0a23a0f32223f1
mPlagge <33815914+mPlagge@users.noreply.github.com>
2021-11-10 10:31:39 +0100

    Updated lib version to git

--- crownstone_uart/core/CrownstoneUart.py
+++ crownstone_uart/core/CrownstoneUart.py
@@ -29,7 +29,7 @@ from crownstone_uart.topics.SystemTopics import SystemTopics
 _LOGGER = logging.getLogger(__name__)
 
 class CrownstoneUart:
-    __version__ = "2.1.0"
+    __version__ = "2.1.0-git"
 
     def __init__(self):
         self.uartManager = None
