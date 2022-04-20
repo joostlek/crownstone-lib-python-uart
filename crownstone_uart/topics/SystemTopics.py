5b29d6124867012336473cd1169f43ab6e8bc83e
Arend de Jonge <arenddejonge@gmail.com>
2022-04-20 19:14:31 +0200

    add new SystemTopic for raw incoming uart data and emit directly after read

--- crownstone_uart/topics/SystemTopics.py
+++ crownstone_uart/topics/SystemTopics.py
@@ -2,8 +2,11 @@
 class SystemTopics:
     newCrownstoneFound    = "newCrownstoneFound"
     stateUpdate           = "stateUpdate"            # used to propagate verified state messages through the system
+    
+    uartNewData           = 'uartNewData'            # Sent when new raw uart data is available. Data is a byte array.
     uartNewPackage        = 'uartNewPackage'         # Sent when a UART packet is received. Data is a UartWrapperPacket.
     uartNewMessage        = 'uartNewMessage'         # Sent when a UART message is received. Data is a UartMessagePacket.
+
     uartWriteData         = 'uartWriteData'          # used to write to the UART. Data is array of bytes.
 
     uartWriteError        = 'uartWriteError'         # used to write to the UART. Data is array of bytes.
