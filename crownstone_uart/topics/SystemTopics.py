260addf4e83329763dcd14d7fbe1d984fa6c23b4
Arend de Jonge <arenddejonge@gmail.com>
2022-04-26 18:50:40 +0200

    add SystemTopics.uartNoiseData and emit said events from UartReadBuffer

--- crownstone_uart/topics/SystemTopics.py
+++ crownstone_uart/topics/SystemTopics.py
@@ -4,6 +4,7 @@ class SystemTopics:
     stateUpdate           = "stateUpdate"            # used to propagate verified state messages through the system
     
     uartRawData           = 'uartRawData'            # Sent when new raw uart data is available. Data is a byte array.
+    uartNoiseData         = 'uartGarbageData'        # Sent when new raw uart data is available, but binary protocol was not expecting anything. Data is a byte array.
     uartNewPackage        = 'uartNewPackage'         # Sent when a UART packet is received. Data is a UartWrapperPacket.
     uartNewMessage        = 'uartNewMessage'         # Sent when a UART message is received. Data is a UartMessagePacket.
 
